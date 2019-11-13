b12d37a6bbb7237ffb16d567400940fc91c774e4
Sara <sara.berling@hotmail.com>
2019-11-13 12:50:17 +0100

    Add project files.

---
+++ Uppgift1UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Uppgift1UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

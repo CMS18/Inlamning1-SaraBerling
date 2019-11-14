214fda5daeece0f579e106e105f4fe9d1f394998
Sara Björnlund <43030248+SaraBjornlund1@users.noreply.github.com>
2019-11-14 15:49:35 +0100

    added transfer method (#3)

---
+++ Uppgift1UI/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Uppgift1UI.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Uppgift1UI.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Transfer(int fromAcc, int toAcc, decimal amount)
+        {
+            var model = new BankRepository();
+            var customers = model.GetAllCustomers();
+            TempData["result"] = "Account was not found.";
+            var fromAccount = customers.SelectMany(c => c.Accounts).SingleOrDefault(a => a.AccountId == fromAcc);
+            var toAccount = customers.SelectMany(c => c.Accounts).SingleOrDefault(a => a.AccountId == toAcc);
+
+            if(fromAccount != null && toAccount != null)
+            {
+                if(amount <= 0)
+                {
+                    TempData["result"] = $"Invalid amount!";
+                }
+                if (fromAccount.Balance < amount)
+                {
+                    TempData["result"] = $"Not enough balance on account!";
+                }
+                else
+                {
+                    TempData["result"] = $"Success! Transfered {amount} :- to account {toAcc}, New balance: {toAccount.Balance}";
+                    fromAccount.Balance -= amount;
+                    toAccount.Balance += amount;
+                }
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Uppgift1UI/Models/Account.cs
+++ Uppgift1UI/Models/Account.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Uppgift1UI.Models;
 
 namespace Uppgift1UI.Models
 {
@@ -9,5 +10,23 @@ namespace Uppgift1UI.Models
     {
         public int AccountId { get; set; }
         public decimal Balance { get; set; }
+
+        public void Transfer(int fromAcc, int TooAcc, decimal amount)
+        {
+            BankRepository bankRepo = new BankRepository();
+            var customers = bankRepo.GetAllCustomers();
+            var fromAccount = customers.SelectMany(c => c.Accounts).SingleOrDefault(a => a.AccountId == fromAcc);
+            var toAccount = customers.SelectMany(c => c.Accounts).SingleOrDefault(a => a.AccountId == TooAcc);
+
+            if (amount > fromAccount.Balance || amount < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else
+            {
+                fromAccount.Balance -= amount;
+                toAccount.Balance += amount;
+            }
+        }
     }
 }
